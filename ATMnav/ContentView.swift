d30deff7457967edd4e35e73d9d6f8408fc91a5e
Taeuk Kang <taeukkang09@gmail.com>
2020-01-18 14:37:06 -0500

    Initial commit

---
+++ ATMnav/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ATMnav
+//
+//  Created by Taeuk Kang on 2020-01-18.
+//  Copyright © 2020 Taeuk Kang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
